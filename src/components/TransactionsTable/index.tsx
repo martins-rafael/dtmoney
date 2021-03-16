e0a1f493fc723e8d84d5b144a15c4456241dcc00
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-16 15:15:15 -0300

    Consume api - config MirageJS

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,32 +1,41 @@
+import { useEffect } from 'react';
 import { Container } from './styles';
 
-export const TransactionsTable = () => (
-  <Container>
-    <table>
-      <thead>
-        <tr>
-          <th>Título</th>
-          <th>Valor</th>
-          <th>Categoria</th>
-          <th>Data</th>
-        </tr>
-      </thead>
+export const TransactionsTable = () => {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, []);
 
-      <tbody>
-        <tr>
-          <td>Desenvolvimento de App</td>
-          <td className="deposit">R$15.000</td>
-          <td>Desenvolvimento</td>
-          <td>16/03/2021</td>
-        </tr>
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
 
-        <tr>
-          <td>X-Bacon</td>
-          <td className="withdraw">-R$25</td>
-          <td>Desenvolvimento</td>
-          <td>10/03/2021</td>
-        </tr>
-      </tbody>
-    </table>
-  </Container>
-);
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de App</td>
+            <td className="deposit">R$15.000</td>
+            <td>Desenvolvimento</td>
+            <td>16/03/2021</td>
+          </tr>
+
+          <tr>
+            <td>X-Bacon</td>
+            <td className="withdraw">-R$25</td>
+            <td>Desenvolvimento</td>
+            <td>10/03/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Services',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
