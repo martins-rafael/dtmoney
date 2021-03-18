3c2670c05912c1ac0ae8ddf93348a60316c28744
Rafael Martins <rafaeldcmartins@gmail.com>
2021-03-17 21:29:32 -0300

    Transaction type button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
 import Modal from 'react-modal';
 
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -32,6 +34,19 @@ export const NewTransactionModal = ({
 
       <input type="text" placeholder="Título" />
       <input type="number" placeholder="Valor" />
+
+      <TransactionTypeContainer>
+        <button type="button">
+          <img src={incomeImg} alt="Entrada" />
+          <span>Entrada</span>
+        </button>
+
+        <button type="button">
+          <img src={outcomeImg} alt="Saída" />
+          <span>Saída</span>
+        </button>
+      </TransactionTypeContainer>
+
       <input type="text" placeholder="Categoria" />
 
       <button type="submit">Cadastrar</button>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.form`
   h2 {
@@ -41,6 +42,40 @@ export const Container = styled.form`
     
     &:hover {
       filter: brightness(0.9);
+    }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+  margin: 1rem 0;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 4rem;
+    background: transparent;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+    transition: border-color 0.2s ease-in-out;
+
+    &:hover {
+      border-color: ${darken(0.1, '#d7d7d7')};
+    }
+
+    img {
+      width: 20px;
+      height: 20px;
+    }
+
+    span {
+      display: block;
+      margin-left: 1rem;
+      font-size: 1rem;
+      color: var(--text-title);
     }
   }
 `;
